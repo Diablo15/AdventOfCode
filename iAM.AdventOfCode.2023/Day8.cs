3c34385d641f067072d8badaa10a66ac5e854907
Alessio Muis <a.muis@chipsoft.com>
2024-12-10 10:27:16 +0100

    Completed day 8 part 2

--- iAM.AdventOfCode.2023/Day8.cs
+++ iAM.AdventOfCode.2023/Day8.cs
@@ -1,10 +1,8 @@
-﻿using iAM.AdventOfCode._2023.Models;
-using iAM.AdventOfCode.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using iAM.AdventOfCode.Helpers;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace iAM.AdventOfCode._2023
 {
@@ -12,19 +10,31 @@ namespace iAM.AdventOfCode._2023
     {
         public Dictionary<string, (string left, string right)> PuzzleMeasurements { get; set; }
         public List<string> Directions { get; set; }
+        public string Directions2 { get; set; }
 
-        public Day8() : base(8, true, false)
+        public Day8() : base(8, false, true)
         {
             this.PuzzleAltFilePath = @"Examples\Day8_small.txt";
             PuzzleMeasurements = new();
+            ReadPuzzleInput();
         }
 
         public override void Puzzle1Content()
         {
-            ReadPuzzleInput();
-            var totalSteps = FollowInstructions();
+            var totalSteps = FollowInstructionsToZZZ();
 
             // Answer: 15871
+            Console.WriteLine($"========= Total steps: {totalSteps} =========");
+        }
+
+        public override void Puzzle2Content()
+        {
+            var lines = FileReader.ReadInputValues<string>(this.Puzzle1FilePath, '\n', true);
+
+            var totalSteps = PartTwo(lines.ToList());
+            //var totalSteps = FollowInstructionsToEndingZ();
+
+            // Answer: 11283670395017
             Console.WriteLine($"========= Total steps: {totalSteps} =========");
         }
 
@@ -43,7 +53,7 @@ namespace iAM.AdventOfCode._2023
             }
         }
 
-        private double FollowInstructions()
+        private double FollowInstructionsToZZZ()
         {
             double steps = 0;
             var i = 0;
@@ -81,9 +91,108 @@ namespace iAM.AdventOfCode._2023
             return steps;
         }
 
-        public override void Puzzle2Content()
+        #region Stolen code Part II
+        private readonly Regex nodeRegex = new Regex("\\w+");
+
+        public record Node(string Id, string Left, string Right);
+
+        private ImmutableDictionary<string, Node> ParseNodes(List<string> input) =>
+            input.Skip(1)
+            .Select(line => nodeRegex.Matches(line)).
+                  Select(parts => new Node(parts[0].Value,
+                                           parts[1].Value,
+                                           parts[2].Value)).
+                  ToImmutableDictionary(node => node.Id);
+
+        private string ParseInstructions(string input) => input.Split("\n")[0];
+
+        public List<string> FindPath(string from,
+                                     Func<Node, bool> condition,
+                                     ImmutableDictionary<string, Node> nodes,
+                                     string instructions)
         {
-            throw new NotImplementedException();
+            var path = new List<string>();
+            var current = nodes[from];
+            var lrCnt = 0;
+
+            while (!condition(current))
+            {
+                path.Add(current.Id);
+                current = nodes[instructions[lrCnt % instructions.Length] == 'L' ? current.Left : current.Right];
+                lrCnt++;
+            }
+
+            return path;
+        }
+
+        private long FindLeastCommonMultiple(IEnumerable<long> numbers) =>
+            numbers.Aggregate((long)1, (current, number) => current / GreatestCommonDivisor(current, number) * number);
+
+        private long GreatestCommonDivisor(long a, long b)
+        {
+            while (b != 0)
+            {
+                a %= b;
+                (a, b) = (b, a);
+            }
+            return a;
+        }
+
+        public object PartTwo(List<string> input)
+        {
+            var instructions = ParseInstructions(input.First());
+            var nodes = ParseNodes(input);
+
+            var deltas = nodes.Values.Where(n => n.Id.EndsWith('A')).
+                                     Select(node => FindPath(node.Id, n => n.Id.EndsWith('Z'), nodes, instructions)).
+                                     Select(path => (long)path.Count);
+
+            return FindLeastCommonMultiple(deltas);
+        }
+        #endregion
+
+        private double FollowInstructionsToEndingZ()
+        {
+            double steps = 0;
+            var i = 0;
+            List<KeyValuePair<string, (string left, string right)>> dirs = new();
+
+            if (steps is 0)
+            {
+                dirs = this.PuzzleMeasurements.Where(k => k.Key.EndsWith('A')).ToList();
+            }
+
+            while (!dirs.TrueForAll(d => d.Key.EndsWith('Z')))
+            {
+                var direction = this.Directions[i];
+                string way = string.Empty;
+
+
+
+                for (var d = 0; d < dirs.Count; d++)
+                {
+                    if (direction is "L")
+                    {
+                        way = dirs[d].Value.left;
+                    }
+                    else if (direction is "R")
+                    {
+                        way = dirs[d].Value.right;
+                    }
+
+                    dirs[d] = this.PuzzleMeasurements.Where(k => k.Key == way).Single();
+                }
+
+                steps++;
+                i++;
+
+                if (i == this.Directions.Count())
+                {
+                    i = 0;
+                }
+            };
+
+            return steps;
         }
     }
 }
