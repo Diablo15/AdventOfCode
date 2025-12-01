9cc1aceb0efe337677ea169739c148824f04a771
Alessio Muis <a.muis@chipsoft.com>
2025-12-01 09:14:48 +0100

    Init2025

---
+++ iAM.AdventOfCode.2025/Day1.cs
@@ -0,0 +1,21 @@
+﻿using iAM.AdventOfCode.Helpers;
+
+namespace iAM.AdventOfCode._2025
+{
+    public class Day1 : AoCDay
+    {
+        public Day1() : base(1, true, false)
+        {
+        }
+
+        public override void Puzzle1Content()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Puzzle2Content()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ iAM.AdventOfCode.2025/Program.cs
@@ -0,0 +1,13 @@
+﻿using iAM.AdventOfCode._2025;
+
+try
+{
+    new Day1().StartDay();
+
+    Console.Read();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Something went wrong: {ex}");
+    throw;
+}
